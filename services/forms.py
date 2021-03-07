b3535d00557ae0e43bebde9c2a4912158b838501
Mikaela Tan <mikaelaatan@users.noreply.github.com>
2021-03-07 18:20:26 +0800

    Use django forms

--- services/forms.py
+++ services/forms.py
@@ -1,24 +1,22 @@
 from django import forms
-
 from .models import Service
 
 SERVICE_TYPE_CHOICES = [
-    ('hair', 'Hair Service'),
-    ('nails', 'Nail Service'),
-    ('skin', 'Skin Service'),
-    ('makeup', 'Semi-permanent Makeup'),
-    ('face', 'Facial Service'),
-    ('eyes', 'Eyebrow/Eyelash Service'),
+    ('Hair', 'Hair Service'),
+    ('Nails', 'Nail Service'),
+    ('Skin', 'Skin Service'),
+    ('Makeup', 'Semi-permanent Makeup'),
+    ('Face', 'Facial Service'),
+    ('Eyes', 'Eyebrow/Eyelash Service'),
 ]
 
-# class ServiceCreateForm(forms.ModelForm):
-#     class Meta:
-#         model = Service
-#         fields = '__all__'
-
-class ServiceForm(forms.Form):
+class ServiceForm(forms.ModelForm):
     servicename = forms.CharField(label='Name')
     servicetype = forms.ChoiceField(label='Service Type',choices=SERVICE_TYPE_CHOICES)
     serviceprice = forms.DecimalField(label='Price',decimal_places=2)
-    serviceduration = forms.DurationField(label='Duration')
-    servicedescription = forms.CharField(label='Description',widget=forms.Textarea(attrs={'rows':3}))
+    serviceduration = forms.DurationField(label='Duration',initial="00:00:00")
+    servicedescription = forms.CharField(required=False,label='Description',widget=forms.Textarea(attrs={'rows':3}))
+
+    class Meta:
+        model = Service
+        fields = '__all__'
