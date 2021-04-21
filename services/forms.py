a8b715ac266177199423371ddb9ad823ba080dce
Mikaela Tan <mikaelaatan@users.noreply.github.com>
2021-04-21 13:05:19 +0800

    Add changes from main to matt branch

--- services/forms.py
+++ services/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Service
+from datetime import datetime, timedelta
 
 SERVICE_TYPE_CHOICES = [
     ('Hair', 'Hair Service'),
@@ -11,17 +12,24 @@ SERVICE_TYPE_CHOICES = [
 ]
 
 class ServiceForm(forms.ModelForm):
-
-    servicename = forms.CharField(label='Name', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    servicetype = forms.ChoiceField(label='Service Type',choices=SERVICE_TYPE_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
-    serviceprice = forms.DecimalField(label='Price',decimal_places=2, widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    serviceduration = forms.DurationField(label='Duration',initial="00:00:00", widget=forms.TimeInput(attrs={'class': 'form-control'}))
-    servicedescription = forms.CharField(required=False,label='Description',widget=forms.Textarea(attrs={'rows':3, 'class': 'form-control'}))
+    servicename = forms.CharField(label='Name',error_messages = {
+                 'unique':"This service already exists. Please try again."})
+    servicetype = forms.ChoiceField(label='Service Type',choices=SERVICE_TYPE_CHOICES)
+    serviceprice = forms.DecimalField(label="Price",decimal_places=2, help_text="Php")
+    serviceduration = forms.DurationField(label='Duration',initial="00:00", help_text="Please use the following format: <em>HH:MM</em>.")
+    servicedescription = forms.CharField(required=False,label='Description',widget=forms.Textarea(attrs={'rows':3}))
 
     class Meta:
         model = Service
         fields = '__all__'
-       
 
-    
+    def clean_servicename(self):
+        return self.cleaned_data['servicename'].capitalize()
 
+    def clean_serviceduration(self):
+        dur = self.cleaned_data['serviceduration']
+        durstr = str(dur)
+        dur_str = durstr[2:]
+        t = datetime.strptime(dur_str,'%H:%M')
+        delta = timedelta(hours=t.hour, minutes=t.minute)
+        return delta
