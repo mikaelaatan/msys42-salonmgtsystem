be9917f11ddd32354a64981b1e71576c8c389fe0
Mikaela Tan <mikaelaatan@users.noreply.github.com>
2021-04-25 16:23:13 +0800

    Some static are in staticfiles

---
+++ staticfiles/js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+ "use strict";
+
+ var siteDatePicker = function() {
+
+   if ( $('.datepicker').length > 0 ) {
+     $('.datepicker').datepicker();
+   }
+
+ };
+ siteDatePicker();
+
+});
