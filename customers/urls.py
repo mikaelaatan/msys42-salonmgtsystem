79e4aab5cdede291a46c597f185dd7004c75593e
mikaelaatan <mikaela.tan@obf.ateneo.edu>
2022-03-08 21:09:17 -0800

    Delete unnecessary files

--- customers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import *
-
-urlpatterns = [
-    path('signup/', signup_view, name = 'signup'),
-    # path('profile/<int:id>/', views.dynamic_lookup_view, name='customer-profile'),
-]
--- staff/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import StaffModel
-
-# Register your models here.
-admin.site.register(StaffModel)
