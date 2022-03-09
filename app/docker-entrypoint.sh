0aac95a499f23f0ee0a1e6209fbdfbfe797553cb
mikaelaatan <mikaela.tan@obf.ateneo.edu>
2022-03-08 21:44:29 -0800

    Merge wait.sh to entrypoint.sh

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+while ! nc -z db 3306 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 1
+done
+echo "MySQL started"
+
 python manage.py makemigrations
 python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
+
+
 exec "$@"
